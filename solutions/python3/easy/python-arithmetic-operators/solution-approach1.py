54118d4a96cd9133d5a5bfcd52237a49ab6c2854
Utsav Chaudhari <utsavchoudhary1692@gmail.com>
2026-09-05 15:33:25 +0530

    ✅ Arithmetic Operators [python3] — approach1

---
+++ solutions/python3/easy/python-arithmetic-operators/solution-approach1.py
@@ -0,0 +1,17 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+# Problem     Arithmetic Operators
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-05, 03:33 p.m.
+# ──────────────────────────────────────────────────
+
+a = int(input())
+b = int(input())
+
+print(a + b)
+print(a - b)
+print(a * b)

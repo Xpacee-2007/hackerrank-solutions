915ace6e8fc6e004d397a8e4e1b74451f522a74e
Utsav Chaudhari <utsavchoudhary1692@gmail.com>
2026-09-05 15:41:28 +0530

    ✅ sWAP cASE [python3] — approach1

---
+++ solutions/python3/easy/swap-case/solution-approach1.py
@@ -0,0 +1,14 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/swap-case/problem?isFullScreen=true
+# Problem     sWAP cASE
+# Difficulty  Easy
+# Subdomain   Strings
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-05, 03:41 p.m.
+# ──────────────────────────────────────────────────
+
+def swap_case(s):
+    return s.swapcase()
+    
